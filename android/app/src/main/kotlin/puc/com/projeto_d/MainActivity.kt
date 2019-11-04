ea59ca59327a232debf23263d06e149d507919bb
gtalles <gtalles@ciandt.com>
2019-11-04 19:40:15 -0300

    initial commit

---
+++ android/app/src/main/kotlin/puc/com/projeto_d/MainActivity.kt
@@ -0,0 +1,13 @@
+package puc.com.projeto_d
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
